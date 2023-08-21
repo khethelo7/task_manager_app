0c585810e50b019c88c0c1f01b6e1fbae2dae415
Khethelo Nkosi <saneleknkosi@gmail.com>
2023-08-21 14:06:03 +0200

    stack(homescreen): Added TextField

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home-screen';
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Task Manager")),
-      body: Center(child: Text("No Tasks Added Yet"),),
+      appBar: AppBar(title: Text("Task Manager"), centerTitle: true,),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("No Tasks Added Yet", style:GoogleFonts.montserrat(fontWeight:FontWeight.w700)),
+          ],
+        ),
+        ),
       floatingActionButton: FloatingActionButton(
         child: Icon(
           Icons.add,
@@ -15,9 +23,38 @@ class HomeScreen extends StatelessWidget {
         onPressed: () => showModalBottomSheet(
           context: context,
           builder: (BuildContext context) => Container(
+            padding: const EdgeInsets.all(10.0),
             height: 500,
             color: Colors.blue,
-            child: Column(children: [Text("Add task")]),
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Add task", style: GoogleFonts.montserrat(color: Colors.white, fontSize: 20.0,)),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).pop(),
+                    child: Icon(Icons.close),
+                  ),
+                ]
+              ),
+              const Divider(
+                color: Colors.lightBlue,
+                thickness: 1.2
+              ),
+              SizedBox(height: 5.0),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    borderSide: BorderSide(color: Colors.blue)
+                  ),
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: "Enter task here",
+                  hintStyle:  GoogleFonts.montserrat(),
+                ),
+              )
+            ]),
           )
         )
       ),
