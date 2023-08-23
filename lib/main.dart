2e72a73be72fddb12e22d327226392ae46c142bb
Khethelo Nkosi <saneleknkosi@gmail.com>
2023-08-23 09:59:21 +0200

    refactor(main): Used const modifier to imrpove perfomance

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@ import './screens/home_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
