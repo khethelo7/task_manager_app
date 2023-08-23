ade124ea01651a7f18988ce7d0b75cc11d7763a9
Khethelo Nkosi <saneleknkosi@gmail.com>
2023-08-23 10:30:32 +0200

    refactor(main): Refactored const modifiers to comply to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 import './screens/home_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
